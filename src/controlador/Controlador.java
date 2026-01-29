ef8f1bbb07e9820ec8711f506c549fca2cc2dd40
alumno <ayoubbenjaddi5@gmail.com>
2026-01-29 10:54:00 +0100

    no me va HikariConfig , arreglar o buscar otra opcion

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -6,20 +6,18 @@ import implementacion.DAOInterfaz;
 import implementacion.DAOMysql;
 
 public class Controlador {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		AlumnoDAO modelo = new DAOMysql();
 		InterfazDAO vista = new DAOInterfaz();
-		
+
 		new Controlador().ejecutar(modelo,vista);
-		
+
 	}
-	
-	
-	public void ejecutar(AlumnoDAO alumno,InterfazDAO interfaz) {
+
+	public void ejecutar(AlumnoDAO alumno, InterfazDAO interfaz) {
 		interfaz.menu(alumno);
-	}	
+	}
 
 }
